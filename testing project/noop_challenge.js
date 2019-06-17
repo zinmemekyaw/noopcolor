e841b6d69fdd7a1e68da2dda51554d34777822d1
zinmemekyaw <zmmkucsmb134@gmail.com>
2019-06-17 15:03:47 +0630

    testing project
    adding text file,change url

--- testing project/noop_challenge.js
+++ testing project/noop_challenge.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     $("#chgcolor").on({
         click: function(){
             $.ajax({
-                url: "http://api.noopschallenge.com/hexbot?count=1"
+                url: "http://api.noopschallenge.com/hexbot"
             }).then(function(data) {
                 $(".bg-1").css("background-color",data.colors[0].value );                
             });
